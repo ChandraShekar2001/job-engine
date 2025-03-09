30c5d31b5d849484dc1b1dad98844ce401e5c43a
ChandraShekar2001 <pvchandu07@gmail.com>
2025-03-09 23:37:15 +0530

    Completed fibonacci job

--- apps/executor/src/app/jobs/fibonacci.consumer.ts
+++ apps/executor/src/app/jobs/fibonacci.consumer.ts
@@ -1,15 +1,18 @@
 import { PulsarConsumer, PulsarClient } from '@job-engine/pulsar';
-import { Message } from 'pulsar-client';
 import { OnModuleInit, Injectable } from '@nestjs/common';
-
+import { FibonacciData } from './fibonacci-data.interface';
+import { iterate } from 'fibonacci';
 @Injectable()
-export class FibinacciConsumer extends PulsarConsumer implements OnModuleInit {
+export class FibinacciConsumer
+  extends PulsarConsumer<FibonacciData>
+  implements OnModuleInit
+{
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient, 'Fibonacci');
   }
 
-  protected async onMessage(message: Message): Promise<void> {
-    console.log('FibonacciConsumer.onMessage');
-    await this.acknowledgeMessage(message);
+  protected async onMessage(data: FibonacciData): Promise<void> {
+    const result = iterate(data.iterations);
+    this.logger.log(result);
   }
 }
